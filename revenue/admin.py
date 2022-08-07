9ff89ca551a3f0de186d8314f957b40d96e5b0f6
Athenas2021 <lucas.kano@outlook.com>
2022-08-07 15:00:46 -0300

    stated overwrite create method in revenue and  expenditure

--- revenue/admin.py
+++ revenue/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
 from .models import Revenue
 
-# TODO - improve admin
+
 admin.site.register(Revenue)
--- revenue/views.py
+++ revenue/views.py
@@ -1,6 +1,7 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from revenue.models import Revenue
 from revenue.serializers import RevenueSerializer
+from rest_framework.response import Response
 
 
 class RevenueViewSet(viewsets.ModelViewSet):
@@ -9,3 +10,15 @@ class RevenueViewSet(viewsets.ModelViewSet):
     '''
     queryset = Revenue.objects.all().order_by('-id')
     serializer_class = RevenueSerializer
+
+    def create(self, request):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        '''
+        TODO - Colocar aqui validação para regra:
+        A API não deve permitir o cadastro de depesas duplicadas
+        (contendo a mesma descrição, dentro do mesmo mês)
+        '''
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
