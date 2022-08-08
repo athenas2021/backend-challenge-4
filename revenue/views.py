3fe3b79d41dc6a4951522c318b4055f58c133bdb
lkdev <lucas.kano@outlook.com>
2022-08-08 20:28:43 -0300

    added revenue create method some business rules

--- revenue/views.py
+++ revenue/views.py
@@ -1,7 +1,9 @@
+
 from rest_framework import viewsets, status
 from revenue.models import Revenue
 from revenue.serializers import RevenueSerializer
 from rest_framework.response import Response
+from rest_framework.exceptions import ValidationError
 
 
 class RevenueViewSet(viewsets.ModelViewSet):
@@ -14,11 +16,13 @@ class RevenueViewSet(viewsets.ModelViewSet):
     def create(self, request):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        '''
-        TODO - Colocar aqui validação para regra:
-        A API não deve permitir o cadastro de depesas duplicadas
-        (contendo a mesma descrição, dentro do mesmo mês)
-        '''
+        
+        month = serializer.validated_data['date'].month
+        list_revenues = Revenue.objects.filter(description=serializer.validated_data['description'])
+        for revenue in list_revenues:
+            if month == revenue.date.month:
+                raise ValidationError(f'Revenue duplicated in this month ({month})', code='duplicated')
+                
         self.perform_create(serializer)
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
