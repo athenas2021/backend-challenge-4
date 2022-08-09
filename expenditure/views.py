4bbbe21d985ebd2481c54ffc4ea57897e498b8d2
lkdev <lucas.kano@outlook.com>
2022-08-09 19:15:13 -0300

    Subscribe expenditure's list method to receive filter

--- expenditure/views.py
+++ expenditure/views.py
@@ -1,6 +1,7 @@
 from rest_framework import viewsets
 from expenditure.models import Expenditure
 from expenditure.serializers import ExpenditureSerializer
+# from datatables.models import DataTables
 
 
 class ExpenditureViewSet(viewsets.ModelViewSet):
@@ -9,3 +10,13 @@ class ExpenditureViewSet(viewsets.ModelViewSet):
     '''
     queryset = Expenditure.objects.all().order_by('-id')
     serializer_class = ExpenditureSerializer
+
+    def list(self, request):
+        queryset = self.get_queryset()
+        print('queryset->', queryset)
+        filter_description = request.GET.getlist('description', None)
+        if len(filter_description) > 0:
+            queryset = queryset.filter(description__in=filter_description)
+        queryset = self.paginate_queryset(queryset)
+        serializer = ExpenditureSerializer(queryset, many=True, context={'request': request})
+        return self.get_paginated_response(serializer.data)
